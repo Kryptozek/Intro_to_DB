471220d858838df16c35a06352e27d8f52212c0e
Kryptozek <youngsammy87@gmail.com>
2024-10-20 13:15:58 +0300

    task 4 commit 2

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,7 @@
 -- task_4.sql
 
 -- Query the information_schema to get the full description of the 'books' table
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'  -- Specify the database
-AND TABLE_NAME = 'books';              -- Specify the table name
+AND TABLE_NAME = 'Books';              -- Specify the table name
