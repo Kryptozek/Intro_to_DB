89fa533c2d08a50df1b0644b68e2e0763d7cbee7
Kryptozek <youngsammy87@gmail.com>
2024-10-20 13:09:24 +0300

    Commit

--- task_4.sql
+++ task_4.sql
@@ -1,6 +1,7 @@
 -- task_4.sql
 
-USE alx_book_store;
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'books';
+-- Query the information_schema to get the full description of the 'books' table
+SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM information_schema.columns
+WHERE TABLE_SCHEMA = 'alx_book_store'  -- Specify the database
+AND TABLE_NAME = 'books';              -- Specify the table name
