0aef40ab980c10f0912df20df9eabaf2f75844bf
Kryptozek <youngsammy87@gmail.com>
2024-10-20 13:13:57 +0300

    task 4 commit

--- task_4.sql
+++ task_4.sql
@@ -2,6 +2,6 @@
 
 -- Query the information_schema to get the full description of the 'books' table
 SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM information_schema.columns
+FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'  -- Specify the database
 AND TABLE_NAME = 'books';              -- Specify the table name
